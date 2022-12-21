cc9001c03c1bab40b08e1ee94e61d8c3a4fb6d06
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2022-12-21 09:46:44 +0100

    Modifications de la video du hero, rédaction des textes

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Psycho</title>
     <link rel="icon" type="image/png" href="images/logo.png">
         <link rel="stylesheet" href="min/style.min.css">
+
         <script src="https://kit.fontawesome.com/eafcd51cfc.js" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -33,6 +34,7 @@
             <video autoplay muted loop>
                 <source src="images/hero.mp4" type="video/mp4">
             </video>
+            <!-- <img src="images/hero-hand.png" alt="main"> -->
             <div class="section__content">        
                 <h1>Leslie Fessel</h1>
                 <h2>Psychomotricienne D.E.</h2> 
@@ -44,12 +46,15 @@
         <section id="about">
             <div class="section__content">
                 <h2>Qui suis-je?</h2>
-                <div class="section__content--max">
-                    <img src="images/portrait.jpg" alt="dessin portrait femme">
+                <div class="section__content--max about">
+                    <img src="images/portrait.jpg" alt="photo Leslie">
+                    <div class="float"></div>
                     <p>
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                        Psychomotricienne diplômée d'État depuis 2017, j'ai effectué ma formation au sein de l'Institut de Formation en Psychomotricité d'Orléans (45).
                         <br>
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. 
+                        Depuis 2017, j'exerce au sein du service de Psychiatrie de l'Enfant et de l'Adolescent du Centre Hospitalier de Blois. J'ai pu intervenir dans différentes unités du service en soins ambulatoires (CMP/Hopital de Jour) mais également en service d'hospitalisation en pédopsychiatrie.
+                        <br>
+                        J'ai notamment développé une pratique clinique auprès de l'adolescent, mais également auprès de sa famille. Ces années d'exercice me permettent aujourd'hui de proposer des séances de thérapies psycho-corporelles destinées aux enfants, aux adolescents et aux adultes. Je propose des consultations individuelles mais également en consultation parent-bébé, enfant ou adolescent pour un travail axé autour de l'accordage relationnel.
                     </p>
                 </div>
             </div>
@@ -58,11 +63,40 @@
             <div class="section__content">
                 <h2>Psychomotricité</h2>
                 <div class="section__content--max">
-                    <img src="images/psychomotricite__img.jpg" alt="enfants en équilibre qui jouent avec des balles">
+                    <img src="images/psycho-img.png" alt="logo psychomotricité">
+                    <div class="float"></div>
                     <p>
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                        La psychomotricité est axée sur la prévention, le dépistage et la rééducation des troubles psychomoteurs, à tout âge de la vie. La.le psychomotricien.ne étudie les interactions entre <b>perception sensorielle, émotion, pensée, mouvement et comportement</b>.
                         <br>
-                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est.
+                        La psychomotricité est également un <b>espace de thérapie</b> basée sur une approche <b>psycho-corporelle</b> par le mouvement, l'expression corporelle, la relaxation, les modelages... visant à rétablir les <b>liens entre le corps et le psychisme</b>.
+                    </p>
+                    <h3>Pour qui ?</h3>
+                    <p>
+                        Les séances de psychomotricité sont soumises à une obligation de prescription médicale (médecin traitant, psychiatre, pédopsychiatre...).
+                        <br>
+                        Les champs d'intervention en psychomotricité sont multiples :
+                    </p>
+                    <ul>
+                        <li>retard du développement psychomoteur</li>
+                        <li>troubles de la latéralité</li>
+                        <li>troubles de l'organisation spatio-temporelle</li>
+                        <li>troubles tonico-émotionnels (coordinations, accordage relationnel parent-enfant par exemple)</li>
+                        <li>inhibition psychomotrice</li>
+                        <li>agitation/instabilité psychomotrice</li>
+                        <li>troubles de l'image du corps et du schéma corporel (dysmorphophobie par exemple)</li>
+                        <li>anxiété, stress, angoisses</li>
+                    </ul>
+                    <h3>Quand faut-il consulter un.e psychomotricien.ne ?</h3>
+                    <p>
+                        La psychomotricité est destinée aux enfants, aux adolescents et aux adultes ayant des difficultés sur le plan sensoriel, moteur, émotionnel, relationnel et/ou comportemental.
+                        <br>
+                        La thérapie psychomotrice est un espace de soin pouvant accueillir la souffrance psychologique lorsqu'elle est vécue et/ou exprimée par le corps. Elle propose un espace de soin fondé sur une approche corporelle, permettant ainsi ; même lorsque les mots vous manquent, de vous reconnecter à vos ressentis internes, vos sensations et vos émotions.
+                    </p>
+                    <h3>Les consultations en psychomotricité</h3>
+                    <p>
+                        La première consultation a pour objectif <!--de créer une première rencontre afin--> de tisser les premiers liens de confiance. Cette première séance permettra également d'établir la nécessité, ou non, d'une passation de bilan psychomoteur, de cerner votre demande et d'élaborer les objectifs des séances. Je pourrai ainsi adapter au mieux le cadre des consultations et mes propositions de soins.
+                        <br>
+                        Le bilan psychomoteur se déroule sur 3 séances d'une heure avec une séance de restitution des résultats (ainsi qu'un compte-rendu écrit). Il a pour but d'analyser le développement psychomoteur de la personne au travers de différents items comme les coordinations, l'équilibre, la latéralité, le tonus, l'espace et le temps, le schéma corporel et l'image du corps. Le bilan psychomoteur peut constituer une première étape diagnostique dans le répérage d'un retard de développement ou de troubles des apprentissages. Les résultats du bilan permettront d'affiner les indications de suivi en psychomotricité ou d'une réorientation thérapeutique.
                     </p>
                 </div>
             </div>
@@ -114,8 +148,11 @@
                             <div class="phone">
                                 <p>06.98.50.41.84</p>
                             </div>
+                            <div class="email">
+                                <p>lesliefessel@gmail.com</p>
+                            </div>
                             <div class="address">
-                                <p>5 Avenue du docteur Jean Laigret</p>
+                                <p>5 Avenue du Docteur Jean Laigret</p>
                                 <p>41000 Blois</p>
                             </div>
                         </div>
@@ -142,6 +179,12 @@
                     </form>
                 </div>
             </div>
+            <p class="consults">
+                Consultations les <b>jeudis de 8h à 19h</b>
+                <br>
+                sur rendez-vous uniquement
+            </p>
+            <img src="images/hands.png" alt="dessins mains ouvertes">
         </section>
         <a href="#hero" id="up">
             <i class="fa-solid fa-circle-chevron-up"></i>
@@ -151,6 +194,7 @@
         <div class="footer__logo">
             <img src="images/logo.png" alt="logo personnage en tailleur">
             <p>Leslie Fessel</p>
+            <span>Psychomotricienne D.E.</span>
         </div>
     </footer>
     <script src="min/script.min.js"></script>
